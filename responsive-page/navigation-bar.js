fc30b42bdb38152903323b7573238d9163037bc3
filyp3r <lyperodrigues03@gmail.com>
2021-02-08 16:13:55 -0200

    Update

--- responsive-page/navigation-bar.js
+++ responsive-page/navigation-bar.js
@@ -1,18 +1,18 @@
 document.write(
 `<div id="nav-bar">
-<div id="bar-buttons">
-    <header>NAME THIS SITE</header>
-    <div id="bar-resp" tabindex="0">
-        <div class="bar"></div>
-        <div class="bar"></div>
-        <div class="bar"></div>
-    </div>
+    <div id="bar-buttons">
+        <header><a href="index.html" id="main-page-redirect"><p>NAME THIS SITE</p></a></header>
+        <div id="bar-resp" tabindex="0">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
 
-    <ul tabindex="0">
-        <li><a href="#"><p>OBJECTIVE</p></a></li>
-        <li><a href="#"><p>PROJECTS</p></a></li>
-        <li><a href="#"><p>MORE</p></a></li>
-    </ul>
-</div>
+        <ul tabindex="0">
+            <li><a href="objective.html"><p>OBJECTIVE</p></a></li>
+            <li><a href="projects.html" ><p>PROJECTS</p></a></li>
+            <li><a href="more.html"     ><p>MORE</p></a></li>
+        </ul>
+    </div>
 </div>`
 );
