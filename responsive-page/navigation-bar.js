dcd98b0cd4698222b102f9181314dd7cb886f94b
filyp3r <lyperodrigues03@gmail.com>
2021-02-08 01:57:20 -0200

    Adicionado devolta a responsive-page

---
+++ responsive-page/navigation-bar.js
@@ -0,0 +1,18 @@
+document.write(
+`<div id="nav-bar">
+<div id="bar-buttons">
+    <header>NAME THIS SITE</header>
+    <div id="bar-resp" tabindex="0">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+
+    <ul tabindex="0">
+        <li><a href="#"><p>OBJECTIVE</p></a></li>
+        <li><a href="#"><p>PROJECTS</p></a></li>
+        <li><a href="#"><p>MORE</p></a></li>
+    </ul>
+</div>
+</div>`
+);
