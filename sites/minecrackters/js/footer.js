e4d561ed63843ce8c4cdbfb2982873859a3034ff
filyp3r <lyperodrigues03@gmail.com>
2021-02-21 00:06:26 -0300

    Update

--- sites/minecrackters/js/footer.js
+++
@@ -1,8 +0,0 @@
-document.write(`
-<footer>
-    <div class="social-links">
-        <a href="https://github.com/filyp3r" target="_blank"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn1.iconfinder.com%2Fdata%2Ficons%2Fsimple-icons%2F1024%2Fgithub-1024-black.png&f=1&nofb=1" alt="Github"></a>
-    </div>
-
-</footer>
-`);
